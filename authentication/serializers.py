fee42ec3db167c251f8ed51dda861c61cfc1e8de
muhnandajr <muhnandajr@gmail.com>
2020-08-17 13:45:13 +0700

    update user profile

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -6,7 +6,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = UserProfile
-        fields = ('nim', 'dob','number_phone', 'address', 'province', 'city', 'zip', 'photo')
+        fields = ('nim', 'dob','number_phone', 'village','rt_village','rw_village', 'province', 'city', 'postal_code', 'photo')
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     profile = UserProfileSerializer(required=True)
@@ -37,10 +37,12 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         profile.nim = profile_data.get('nim', profile.nim)
         profile.dob = profile_data.get('dob', profile.dob)
         profile.number_phone = profile_data.get('number_phone', profile.number_phone)
-        profile.address = profile_data.get('address', profile.address)
+        profile.village = profile_data.get('village', profile.village)
+        profile.rt_village = profile_data.get('rt_village', profile.rt_village)
+        profile.rw_village = profile_data.get('rw_village', profile.rw_village)
         profile.province = profile_data.get('province', profile.province)
         profile.city = profile_data.get('city', profile.city)
-        profile.zip = profile_data.get('zip', profile.zip)
+        profile.postal_code = profile_data.get('postal_code', profile.postal_code)
         profile.photo = profile_data.get('photo', profile.photo)
         profile.save()
 
